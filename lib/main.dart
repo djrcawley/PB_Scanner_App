4ed113e15f4508ef6d1bb30818a734a16f1d0e5b
Yu Ge <yu.ge@uconn.edu>
2022-11-27 17:17:12 -0500

    modified:   ios/Flutter/Debug.xcconfig 	modified:   ios/Flutter/Release.xcconfig 	new file:   ios/Podfile 	new file:   ios/Podfile.lock 	modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner.xcworkspace/contents.xcworkspacedata 	modified:   lib/main.dart 	modified:   macos/Flutter/Flutter-Debug.xcconfig 	modified:   macos/Flutter/Flutter-Release.xcconfig 	new file:   macos/Podfile 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@
 
 import 'package:flutter/material.dart';
 
+//for camera page
+import 'package:camera/camera.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
 void main() {
+  //initing camera page required materials
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(
     title: 'Navigation Basics',
     home: LoginPage(),
@@ -57,13 +63,16 @@ class FirstRoute extends StatelessWidget {
             )
           ],
         ),
+        //bottom right camera button
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Camera_p()),
-              );
+            onPressed: () async {
+              await availableCameras().then((cameras) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CameraPage(cameras: cameras)));
+              });
             }));
   }
 }
@@ -219,3 +228,111 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+
+//the following classes are for Camera Page
+class CameraPage extends StatefulWidget {
+  final List<CameraDescription>? cameras;
+  const CameraPage({this.cameras, Key? key}) : super(key: key);
+
+  @override
+  _CameraPageState createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController controller;
+  XFile? pictureFile;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(
+      widget.cameras![0],
+      ResolutionPreset.max,
+    );
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return const SizedBox(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: CameraPreview(controller),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () async {
+              XFile? pictureFile = await controller.takePicture();
+              setState(() {});
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DisplayPictureScreen(
+                    imagePath: pictureFile!.path,
+                  ),
+                ),
+              );
+            },
+            child: const Text('Capture'),
+          ),
+        ),
+
+        // ImagePath = pictureFile!.path,
+
+        // Image.network(
+        //   pictureFile!.path,
+        //   height: 200,
+        // )
+        //Android/iOS
+        // Image.file(File(pictureFile!.path)))
+      ],
+    );
+  }
+}
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Uploading...')),
+      // The image is stored as a file on the device. Use the `Image.file`
+      // constructor with the given path to display the image.
+      // body: Image.file(File(imagePath)),
+      body: Center(
+        child: LoadingAnimationWidget.staggeredDotsWave(
+          color: Colors.blue,
+          size: 200,
+        ),
+      ),
+    );
+  }
+}
+
+
+//camera page ends
