8c36252f1452c46b05920b5304c81f47854c52de
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-02-24 11:22:02 -0500

    Shrink Text to Box

--- lib/result.dart
+++ lib/result.dart
@@ -1,54 +1,44 @@
-
 import 'screens.dart';
 
 class Result extends StatefulWidget {
-  final Map<String,dynamic> jsonMap;
+  final Map<String, dynamic> jsonMap;
   const Result({super.key, required this.jsonMap});
 
   @override
   State<Result> createState() => ResultState();
 }
 
-class ResultState extends State<Result>
-{
-  String displayStr = 'Score\n\n';
-  
+class ResultState extends State<Result> {
+  String displayStr = 'Successful Scan!\n\n';
+
   @override
   void initState() {
     super.initState();
     buildList();
   }
 
-  void buildList() async
-  {
-    for(var entry in widget.jsonMap.entries)
-    {
+  void buildList() async {
+    for (var entry in widget.jsonMap.entries) {
       await Future.delayed(Duration(seconds: 1));
-      displayStr = displayStr + entry.value.toString()+'\n';
+      displayStr = displayStr + entry.value.toString() + '\n';
       setState(() {});
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Success!'),
-      ),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget> [
-            Text(displayStr, style: TextStyle(fontSize: 18),textAlign: TextAlign.center,),
-            Image.asset(height: 100,
-                        width: 100,
-                        'assets/images/box.png')
-          ],
-        ),          
-      )
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min, //Shrink to Size
+      children: <Widget>[
+        Text(
+          displayStr,
+          style: const TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        Image.asset(height: 100, width: 100, 'assets/images/box.png')
+      ],
     );
   }
 }
