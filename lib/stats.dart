21376950b249c406300ac1d884b03ed5ac48a74d
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-04-06 12:55:34 -0400

    Create Stat Class

--- lib/stats.dart
+++ lib/stats.dart
@@ -12,7 +12,7 @@ const Color blue = Color(0xFF28c2ff);
 List<Color> gradientColors = [primary];
 
 class StatsPage extends StatefulWidget {
-  StatsPage({super.key});
+  const StatsPage({super.key});
 
   @override
   State<StatsPage> createState() {
@@ -27,148 +27,93 @@ class _StatsPageState extends State<StatsPage> {
         backgroundColor: grey.withOpacity(0.25),
         body: SingleChildScrollView(
             child: Column(
-          children: [
+          children: const [
             SizedBox(
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: grey.withOpacity(0.01),
-                        spreadRadius: 10,
-                        blurRadius: 3,
-                        // changes position of shadow
-                      ),
-                    ]),
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Total Points Earned",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 13,
-                                color: Color(0xff67727d),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              child: Image.asset(
-                                height: 100,
-                                width: 100,
-                                'assets/images/badge.png',
-                              ),
-                            ),
-                            Text(
-                              "2645 Points",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                              ),
-                            ),
-                            Text(
-                              "Average: 265 Points",
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 13,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            StatBox(title: "Total Points Earned", stat: "2645 Points", image: 'assets/images/badge.png', average: "Average: 265 Points"),
             SizedBox(
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: white,
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: grey.withOpacity(0.01),
-                        spreadRadius: 10,
-                        blurRadius: 3,
-                        // changes position of shadow
-                      ),
-                    ]),
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 10,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Total Packages Scanned",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 13,
-                                color: Color(0xff67727d),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              child: Image.asset(
-                                height: 100,
-                                width: 100,
-                                'assets/images/box.png',
-                              ),
-                            ),
-                            Text(
-                              "16 Packages",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                              ),
-                            ),
-                            Text(
-                              "Average: 2 Packages",
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 13,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            StatBox(title: "Total Packages Scanned", stat: "16 Packages", image: 'assets/images/box.png', average: "Average: 2 Packages"),
           ],
         )));
   }
+}
+
+class StatBox extends StatelessWidget {
+ final String title;
+ final String stat;
+ final String average;
+ final String image;
+ const StatBox({super.key, required this.title, required this.stat, required this.image, required this.average});
+
+ @override
+ Widget build(BuildContext context) {
+   return Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: grey.withOpacity(0.01),
+                        spreadRadius: 10,
+                        blurRadius: 3,
+                        // changes position of shadow
+                      ),
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 10,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 13,
+                                color: Color(0xff67727d),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Image.asset(
+                              height: 100,
+                              width: 100,
+                              image,
+                            ),
+                            Text(
+                              stat,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                              ),
+                            ),
+                            Text(
+                              average,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 13,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+ }
 }
