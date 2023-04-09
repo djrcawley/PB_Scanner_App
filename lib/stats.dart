130d6e7481c4fb9d2bd87ba30b7b4dbc6c4f86da
user1243244132423 <118788567+user1243244132423@users.noreply.github.com>
2023-04-09 19:48:25 -0400

    Progress Bars

--- lib/stats.dart
+++ lib/stats.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'screens.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 const Color primary = Color(0xFFFF3378);
 const Color secondary = Color(0xFFFF2278);
@@ -23,10 +26,16 @@ class _StatsPageState extends State<StatsPage> {
   String avgPoints = '0';
   String streak = '0';
   String avgStreak = '0';
-  String team = 'Uconn';
-  String teamPoints = '12345';
+  String team = '';
+  String teamPoints = '0';
   String packages = '0';
   String avgPackages = '0';
+  double pointPercent = 0.0;
+  double avgPercent = 0.0;
+  double streakPercent = 0.0;
+  double avgStreakPercent = 0.0;
+  double packagePercent = 0.0;
+  double avgPackagePercent = 0.0;
   @override
   void initState() {
     super.initState();
@@ -61,6 +70,20 @@ class _StatsPageState extends State<StatsPage> {
     avgPackages = responseJson['average_stats']['average_packages'].toString();
     team = responseJson['team']['team'].toString();
     teamPoints = responseJson['team']['points'].toString();
+    double maxPoints = max(double.parse(points), double.parse(avgPoints));
+    pointPercent = double.parse(points) / maxPoints;
+    avgPercent = double.parse(avgPoints) / maxPoints;
+
+    double maxPackage = max(double.parse(packages), double.parse(avgPackages));
+    packagePercent = double.parse(packages) / maxPackage;
+    avgPackagePercent = double.parse(avgPackages) / maxPackage;
+
+    double maxStreak = max(double.parse(streak), double.parse(avgStreak));
+    streakPercent =
+        (double.parse(streak) == 0.0) ? 0 : double.parse(streak) / maxStreak;
+    avgStreakPercent = (double.parse(avgStreak) == 0.0)
+        ? 0
+        : double.parse(avgStreak) / maxStreak;
     setState(() {});
     return true;
   }
@@ -76,34 +99,54 @@ class _StatsPageState extends State<StatsPage> {
               height: 10,
             ),
             StatBox(
-                title: "Total Points Earned",
-                stat: "$points Points",
-                image: 'assets/images/badge.png',
-                average: "Average: $avgPoints Points"),
+              title: "Total Points Earned",
+              statTitle: "$points Points",
+              image: 'assets/images/badge.png',
+              averageTitle: "Average: $avgPoints Points",
+              statPlain: points,
+              averagePlain: avgPoints,
+              user: pointPercent,
+              avg: avgPercent,
+              showChart: true,
+            ),
             SizedBox(
               height: 10,
             ),
             StatBox(
-                title: "Total Packages Scanned",
-                stat: "$packages Packages",
-                image: 'assets/images/box.png',
-                average: "Average: $avgPackages Packages"),
+              title: "Total Packages Scanned",
+              statTitle: "$packages Packages",
+              image: 'assets/images/box.png',
+              averageTitle: "Average: $avgPackages Packages",
+              statPlain: packages,
+              averagePlain: avgPackages,
+              user: packagePercent,
+              avg: avgPackagePercent,
+              showChart: true,
+            ),
             SizedBox(
               height: 10,
             ),
             StatBox(
-                title: "Daily Streak",
-                stat: "$streak Days",
-                image: 'assets/images/streak.png',
-                average: "Average: $avgStreak Days"),
+              title: "Daily Streak",
+              statTitle: "$streak Days",
+              image: 'assets/images/streak.png',
+              averageTitle: "Average: $avgStreak Days",
+              statPlain: streak,
+              averagePlain: avgStreak,
+              user: streakPercent,
+              avg: avgStreakPercent,
+              showChart: true,
+            ),
             SizedBox(
               height: 10,
             ),
             StatBox(
-                title: "Team",
-                stat: "$team",
-                image: 'assets/images/team.png',
-                average: "Team Points: $teamPoints"),
+              title: "Team",
+              statTitle: "$team",
+              image: 'assets/images/team.png',
+              averageTitle: "Team Points: $teamPoints",
+              showChart: false,
+            ),
           ],
         )));
   }
@@ -111,15 +154,28 @@ class _StatsPageState extends State<StatsPage> {
 
 class StatBox extends StatefulWidget {
   final String title;
-  final String stat;
-  final String average;
+  final String statTitle;
+  final String averageTitle;
+  final String? statPlain;
+  final String? averagePlain;
   final String image;
-  const StatBox(
+  final double? user;
+  final double? avg;
+  final bool showChart;
+
+  bool isChartVisible = false;
+
+  StatBox(
       {super.key,
       required this.title,
-      required this.stat,
-      required this.average,
-      required this.image});
+      required this.image,
+      this.user,
+      this.avg,
+      required this.showChart,
+      required this.statTitle,
+      required this.averageTitle,
+      this.statPlain,
+      this.averagePlain});
 
   @override
   State<StatBox> createState() {
@@ -132,70 +188,154 @@ class _StatBox extends State<StatBox> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20),
-      child: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: white,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: grey.withOpacity(0.01),
-                spreadRadius: 10,
-                blurRadius: 3,
-                // changes position of shadow
-              ),
-            ]),
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            widget.isChartVisible = !widget.isChartVisible;
+          });
+        },
         child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      widget.title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 13,
-                        color: Color(0xff67727d),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: grey.withOpacity(0.01),
+                        spreadRadius: 10,
+                        blurRadius: 3,
+                        // changes position of shadow
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Image.asset(
-                      height: 100,
-                      width: 100,
-                      widget.image,
-                    ),
-                    Text(
-                      widget.stat,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 10,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 13,
+                                color: Color(0xff67727d),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Image.asset(
+                              height: 100,
+                              width: 100,
+                              widget.image,
+                            ),
+                            Text(
+                              widget.statTitle,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                              ),
+                            ),
+                            Text(
+                              widget.averageTitle,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 13,
+                              ),
+                            ),
+                            if (widget.showChart && widget.isChartVisible) ...{
+                              const Divider(
+                                indent: 5,
+                                endIndent: 5,
+                                color: Color.fromARGB(255, 78, 78, 78),
+                                thickness: 0.5,
+                              ),
+                              Column(
+                                children: <Widget>[
+                                  const Padding(
+                                      padding: EdgeInsets.only(top: 10)),
+                                  LinearPercentIndicator(
+                                    alignment: MainAxisAlignment.center,
+                                    width: 200,
+                                    lineHeight: 15,
+                                    percent: widget.user!,
+                                    leading: Container(
+                                      width: 50,
+                                      child: Text(
+                                        "User",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    trailing: Container(
+                                      width: 50,
+                                      child: Text(
+                                        widget.statPlain!,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    progressColor: Colors.red,
+                                    backgroundColor: Colors.transparent,
+                                    barRadius: Radius.circular(5),
+                                  ),
+                                  const Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 10, bottom: 10)),
+                                  LinearPercentIndicator(
+                                    alignment: MainAxisAlignment.center,
+                                    width: 200,
+                                    lineHeight: 15,
+                                    percent: widget.avg!,
+                                    leading: Container(
+                                      width: 50,
+                                      child: Text(
+                                        "Average",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    trailing: Container(
+                                      width: 50,
+                                      child: Text(
+                                        widget.averagePlain!,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    progressColor: Colors.orange,
+                                    backgroundColor: Colors.transparent,
+                                    barRadius: Radius.circular(5),
+                                  ),
+                                  const Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 10, bottom: 10)),
+                                ],
+                              ),
+                            }
+                          ],
+                        ),
                       ),
-                    ),
-                    Text(
-                      widget.average,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 13,
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ));
   }
 }
