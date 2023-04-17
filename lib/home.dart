9ec901e375a2d5062f823e30707e060fdd8ef7a5
Max <max.serino@uconn.edu>
2023-04-16 22:50:08 -0400

    Account page

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
 import 'screens.dart';
+import 'package:http/http.dart' as http;
+import 'package:simple_autocomplete_formfield/simple_autocomplete_formfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class HomePage extends StatefulWidget {
   final CameraDescription camera;
@@ -31,7 +36,7 @@ class FirstRoute extends State<HomePage> {
             username: widget.username,
           ),
           const MStatWid(),
-          Settings(camera: widget.camera),
+          Settings(camera: widget.camera, username: widget.username),
         ].elementAt(_selectedIndex), //New
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -65,13 +70,10 @@ class FirstRoute extends State<HomePage> {
 
 // ignore: camel_case_types
 
-
 class Settings extends StatefulWidget {
   final CameraDescription camera;
-  const Settings({
-    super.key,
-    required this.camera,
-  });
+  final String username;
+  const Settings({super.key, required this.camera, required this.username});
 
   @override
   State<Settings> createState() {
@@ -90,7 +92,15 @@ class _Settings extends State<Settings> {
       ),
       body: ListView(children: [
         ListTile(
-            leading: Icon(Icons.account_box_outlined), title: Text("Account")),
+            leading: Icon(Icons.account_box_outlined),
+            title: Text("Account"),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => passBar(username: widget.username)),
+              );
+            }),
         ListTile(
             leading: Icon(Icons.private_connectivity),
             title: Text("Privacy & Security")),
@@ -126,4 +136,195 @@ class MStatWid extends StatelessWidget {
         ),
         body: const StatsPage());
   }
+}
+
+class passBar extends StatelessWidget {
+  final String username;
+  const passBar({super.key, required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Account'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.person)),
+                Tab(icon: Icon(Icons.groups)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              UserPage(username: username),
+              PassPage(username: username),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PassPage extends StatefulWidget {
+  final String username;
+  const PassPage({super.key, required this.username});
+
+  @override
+  State<PassPage> createState() {
+    return _passpage();
+  }
+}
+
+class _passpage extends State<PassPage> {
+  final _formKey = GlobalKey<FormState>();
+  final newPass = TextEditingController();
+  final OldPass = TextEditingController();
+  bool inTeam = false;
+  bool nonExistantTeam = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: SingleChildScrollView(
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, right: 15, top: 15),
+                            child: TextFormField(
+                              controller: OldPass,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  prefixIcon: Icon(Icons.group),
+                                  labelText: 'Password',
+                                  hintText: 'Enter Current Password'),
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, right: 15, top: 15),
+                            child: TextFormField(
+                              controller: newPass,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  prefixIcon: Icon(Icons.group),
+                                  labelText: 'New Password',
+                                  hintText: 'Enter New Password'),
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Container(
+                              height: 50,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: TextButton(
+                                onPressed: () async {
+                                  ChangePass(widget.username, OldPass, newPass);
+                                },
+                                child: const Text(
+                                  'Change Password',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 25),
+                                ),
+                              ),
+                            )),
+                      ],
+                    )))));
+  }
+}
+
+class UserPage extends StatefulWidget {
+  final String username;
+  const UserPage({super.key, required this.username});
+
+  @override
+  State<UserPage> createState() {
+    return _Userpage();
+  }
+}
+
+class _Userpage extends State<UserPage> {
+  final _formKey = GlobalKey<FormState>();
+  final newUser = TextEditingController();
+  bool inTeam = false;
+  bool nonExistantTeam = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: SingleChildScrollView(
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, right: 15, top: 15),
+                            child: TextFormField(
+                              controller: newUser,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  prefixIcon: Icon(Icons.group),
+                                  labelText: 'New Username',
+                                  hintText: 'Enter New Username'),
+                            )),
+                        Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Container(
+                              height: 50,
+                              width: 200,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: TextButton(
+                                onPressed: () async {
+                                  UserName(widget.username, newUser);
+                                },
+                                child: const Text(
+                                  'Change Username',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 25),
+                                ),
+                              ),
+                            )),
+                      ],
+                    )))));
+  }
+}
+
+Future<String> ChangePass(username, oldpass, newpass) async {
+  Uri uri = Uri.parse('https://sdp23.cse.uconn.edu/change-password');
+  final response = await http.post(uri,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': oldpass,
+        'newPassword': newpass
+      }));
+
+  return response.body;
+}
+
+Future<String> UserName(username, newUser) async {
+  Uri uri = Uri.parse('https://sdp23.cse.uconn.edu/change-password');
+  final response = await http.post(uri,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(
+          <String, String>{'username': username, 'newUsername': newUser}));
+
+  return response.body;
 }
